43d09d168bceb9d8d7c7e45c3b09bf4331a36bb6
jennmez <jennmez@gmail.com>
2020-04-26 20:17:38 -0500

    update port

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 const app = require('./server');
-const PORT = 1337;
+const PORT = app.listen(process.env.PORT || 3000);
 
 app.listen(PORT, () => console.log(`your canvas awaits at port ${PORT}`));
